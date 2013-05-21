9ec0191a007edb19db6aeb442c2303e4f5ce5e66
StevenDufresne <stevedufresne2.0@gmail.com>
2013-05-21 18:47:30 -0400

    Cleaned Legacy

--- basic-wordpress/header.php
+++ basic-wordpress/header.php
@@ -4,6 +4,5 @@
   <meta charset="utf-8">
   <title></title>
   <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
-  <script src="<?php sd();?>/js/modernizr.min.js"></script>
   <?php wp_head();?>
 </head>
